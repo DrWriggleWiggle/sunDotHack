cea7d7ec36c6373c0cadb77bdf150a7e7007c268
bull313 <blladi313@gmail.com>
2018-04-07 12:41:00 -0700

    debug modularization

--- sql.php
+++ sql.php
@@ -11,11 +11,11 @@
     $friends = getTable("friends WHERE (friend2='" . $id . "' OR friend1='" . $id . "') AND accepted='1';");
     $date = array();
     foreach ($friends as $f) {
-      echo "<h1>Done</h1>";
       $member = getMemberById($f['friend1']);
-      if ($member['memberId'] == $id) {
-        array_push($data, getMemberById($f['friend2']));
+      if ($member['memberId'] == $_SESSION['id']) {
+        $member = getMemberById($f['friend2']);
       }
+      array_push($data, $member);
     }
     return $data;
   }
