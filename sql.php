70a0883efe02e15e10c41f8981b1e813e633d02e
bull313 <blladi313@gmail.com>
2018-04-07 12:37:59 -0700

    debugging

--- sql.php
+++ sql.php
@@ -11,6 +11,7 @@
     $friends = getTable("friends WHERE (friend2='" . $id . "' OR friend1='" . $id . "') AND accepted='1';");
     $date = array();
     foreach ($friends as $f) {
+      echo "<h1>Done</h1>";
       $member = getMemberById($f['friend1']);
       if ($member['memberId'] == $id) {
         array_push($data, getMemberById($f['friend2']));
