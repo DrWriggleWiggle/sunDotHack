f3890f159044288c2e982d92589a3a22f7ad5651
bull313 <blladi313@gmail.com>
2018-04-07 16:34:31 -0700

    merging bootstrap

--- login.php
+++ login.php
@@ -1,26 +1,34 @@
-<div class="container">
-    <div class="card card-login mx-auto mt-5">
-        <div class="card-header">Login</div>
-        <div class="card-body">
-            <form action="signed_out.php" method="post">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email</label>
-                    <input class="form-control" id="exampleInputEmail1" type="text" name="email" aria-describedby="emailHelp" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password" name="password">
-                </div>
-      <input class="btn btn-primary btn-block" name="submit_login" value="Login" type="submit" />
-            </form>
-            <div class="text-center">
-                <a class="d-block small mt-3" href="register.php">Register an Account</a> <!-- Need to add link -->
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Bootstrap core JavaScript-->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- Core plugin JavaScript-->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<!DOCTYPE html>
+<html>
+<head>
+  <?php require_once("head.php");?>
+<html lang="en">
+<body>
+  <div class="container">
+      <div class="card card-login mx-auto mt-5">
+          <div class="card-header">Login</div>
+          <div class="card-body">
+              <form action="index.php" method="post">
+                  <div class="form-group">
+                      <label for="exampleInputEmail1">Email</label>
+                      <input class="form-control" id="exampleInputEmail1" type="text" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                      <label for="exampleInputPassword1">Password</label>
+                      <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password" name="password">
+                  </div>
+        <input class="btn btn-primary btn-block" name="submit_login" value="Login" type="submit" />
+              </form>
+              <div class="text-center">
+                  <a class="d-block small mt-3" href="register.php">Register an Account</a> <!-- Need to add link -->
+              </div>
+          </div>
+      </div>
+  </div>
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+</body>
+</html>
--- signed_out.php
+++ signed_out.php
@@ -9,7 +9,7 @@ if (isset($_POST['submit_login'])) { // if an attempt to log in has been made, v
     $_SESSION['id'] = $row['memberId'];
     $_SESSION['email'] = $row['email'];
     header("Refresh:0");
-    
+
   }
   else {
     echo "<script type=\"text/javascript\">
@@ -18,35 +18,6 @@ if (isset($_POST['submit_login'])) { // if an attempt to log in has been made, v
   }
 }
 ?>
-
-<!-- Log in form -->
-<div class="container">
-    <div class="card card-login mx-auto mt-5">
-        <div class="card-header">Login</div>
-        <div class="card-body">
-            <form action="index.php" method="post">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email</label>
-                    <input class="form-control" id="exampleInputEmail1" type="text" name="email" aria-describedby="emailHelp" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password" name="password">
-                </div>
-      <input class="btn btn-primary btn-block" name="submit_login" value="Login" type="submit" />
-            </form>
-            <div class="text-center">
-                <a class="d-block small mt-3" href="register.php">Register an Account</a> <!-- Need to add link -->
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Bootstrap core JavaScript-->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- Core plugin JavaScript-->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-<!-- -->
 
 <!-- This will be the default page that appears if the user is not signed in -->
 <body id="page-top">
@@ -63,7 +34,7 @@ if (isset($_POST['submit_login'])) { // if an attempt to log in has been made, v
                         <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="register.php" id="containerLandingPageText" style="color:white">Register</a>
                     </li>
                     <li class="nav-item mx-0 mx-lg-1">
-                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.php" id="containerLandingPageText" style="color:white">Login</a>
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="login.php" id="containerLandingPageText" style="color:white">Login</a>
                     </li>
                 </ul>
             </div>
