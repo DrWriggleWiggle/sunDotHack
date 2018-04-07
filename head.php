3ff62a362941cf39c70dd7361615cd7f75136139
bull313 <blladi313@gmail.com>
2018-04-07 00:06:04 -0700

    created a template for website

---
+++ head.php
@@ -0,0 +1,4 @@
+<title>Social Calendar</title>
+<meta charset="utf-8">
+<meta author="DrWriggleWiggle">
+<link rel="stylesheet" href="style.css">
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<?php require_once("head.php");?>
+<head>
+</head>
+<body>
+  <h1>Dr. Wriggle Wiggle</h1>
+</body>
+</html>
