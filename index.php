92b7b40840b9bbcafd57f9aea5e584f402b883a6
aharken <31554609+aharken@users.noreply.github.com>
2018-04-07 10:09:24 -0700

    All hell will break loose

--- index.php
+++ index.php
@@ -2,6 +2,20 @@
 <html>
 <?php require_once("head.php");?>
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Social Calendar</title>
+  <!-- Bootstrap core CSS-->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Page level plugin CSS-->
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
 </head>
 <body>
   <h1>Dr. Wriggle Wiggle</h1>
