9210b40c6f601d0d9bff7d3c12f17c52ff02780a
bull313 <blladi313@gmail.com>
2018-04-07 13:54:07 -0700

    fixed the add feature

--- signed_in.php
+++ signed_in.php
@@ -176,7 +176,7 @@ echo "<h2>Logged in as $name.</h2>";
           ?>
           <option value="1">Friend1</option>
         </select>
-        <input type="submit" value="Add Event">
+        <input type="submit" name="submit_add_event" value="Add Event">
       </form>
   </div>
   </div>
