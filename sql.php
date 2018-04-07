7716401183934496618c440dcf59f4179a95ad44
bull313 <blladi313@gmail.com>
2018-04-07 00:24:35 -0700

    created website template and some streamlined php/sql code

---
+++ sql.php
@@ -0,0 +1,39 @@
+<?php
+  $dbc = mysqli_connect("localhost", "root", "", "socialcalendar") or die("<h1>Error Connecting to Database</h1>");
+  function query($q_string) {
+    global $dbc;
+    $result = mysqli_query($dbc, $q_string) or die ("<h1>Error with following query:</h1>\n<h2>$q_string</h2>");
+    return $result;
+  }
+
+  function getLastRow($table) {
+    $q = "SELECT * FROM $table ORDER BY id DESC LIMIT 1";
+    $result = query($q);
+    $row = mysqli_fetch_assoc($result);
+    return $row;
+  }
+
+  function getFirstRow($table) {
+    $q = "SELECT * FROM $table ORDER BY id LIMIT 1";
+    $result = query($q);
+    $row = mysqli_fetch_assoc($result);
+    return $row;
+  }
+
+  function getTable($table) {
+    $q = "SELECT * FROM $table";
+    $result = query($q);
+    $data = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+      array_push($data, $row);
+    }
+    return $data;
+  }
+
+  function getById($table, $val) {
+    $request = query("SELECT * FROM $table WHERE id='$val'");
+    if ($row = mysqli_fetch_assoc($request)) {
+      return $row;
+    }
+  }
+?>
