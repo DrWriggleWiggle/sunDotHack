7576160efb63b5b20fe1c9aef1012d0dbe2d5e6e
aharken <31554609+aharken@users.noreply.github.com>
2018-04-07 11:56:27 -0700

    Update

--- signed_out.php
+++ signed_out.php
@@ -66,34 +66,6 @@
 
 <!-- Older PHP Code - to be inserted into bootstrap code -->
 
-<?php
-require_once("sql.php");
-if (isset($_POST['submit_login'])) { // if an attempt to log in has been made, verify. Refresh the page or throw an error message
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $verify_login = query("SELECT * FROM members WHERE email='$email' AND password=SHA('$password')");
-  if ($row = mysqli_fetch_assoc($verify_login)) {
-    $_SESSION['user'] = $row['firstName'] . ' ' . $row['lastName'];
-    $_SESSION['id'] = $row['memberId'];
-    $_SESSION['email'] = $row['email'];
-    header("Refresh:0");
-  } else {
-    echo "<h3><em>Invalid Login</em></h3>";
-  }
-} else if (isset($_POST['submit_register'])) { // if a registration has been submitted, create account adn refresh
-  $first_name = $_POST["firstName"];
-  $last_name = $_POST["lastName"];
-  $email = $_POST["email"];
-  $password = $_POST["password"];
-  query(
-    "INSERT INTO members (firstName, lastName, email, password)
-    VALUES ('$first_name', '$last_name', '$email', SHA('$password'));
-    "
-  );
-  header("Refresh:0");
-}
-?>
-
 <!-- Registration Form -- Will be implemented on a separate page -->
 <!--
 <h2>...Or Create an Account!</h2>
