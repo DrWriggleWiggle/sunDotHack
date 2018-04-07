d84cce9ae1e6f2b53fd6db216177d1cd2102914a
bull313 <blladi313@gmail.com>
2018-04-07 16:55:59 -0700

    fixing login issue

--- login.php
+++ login.php
@@ -14,7 +14,8 @@
       $_SESSION['user'] = $row['firstName'] . ' ' . $row['lastName'];
       $_SESSION['id'] = $row['memberId'];
       $_SESSION['email'] = $row['email'];
-      header("Location: http://167.99.168.175/index.php");
+      header("Location: index.php");
+      exit;
     }
     else {
       echo "<script type=\"text/javascript\">
