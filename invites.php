0fe719d1274f887625db61602a28493ff52c4591
bull313 <blladi313@gmail.com>
2018-04-08 10:27:21 -0700

    debugging

--- invites.php
+++ invites.php
@@ -116,6 +116,7 @@
                       if (isset($_POST['submit_invite_accept'])){ //if invite is accepted, set accepted to 1
                       //$invitations is an array of pending events associated with the current user
                       $invitations = getInvites($_SESSION['id']);
+                      echo count($invitations);
                       foreach ($invitations as $i) {
                         echo "<tr>";
                         echo "<th>Invitation to " . $i['name'] . "</th>";
