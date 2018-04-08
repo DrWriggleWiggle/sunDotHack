64f08c9311ffd80961f7a43d42b7b8a2d5a0639f
bull313 <blladi313@gmail.com>
2018-04-08 09:24:37 -0700

    debugging

--- addfriends.php
+++ addfriends.php
@@ -27,7 +27,7 @@
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">Wriggle Social Calendar</a>
+    <a class="navbar-brand" href="index.php">Wriggle Social Calendar</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
