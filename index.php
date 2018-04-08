0a8ae7447a46ff3691dca8c915bebd47059a9c2b
bull313 <blladi313@gmail.com>
2018-04-07 17:16:24 -0700

    fixing login issue

--- index.php
+++ index.php
@@ -6,6 +6,26 @@
 <?php
 session_start();
 
+require_once("sql.php");
+if (isset($_POST['submit_login'])) { // if an attempt to log in has been made, verify. Refresh the page or throw an error message
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $verify_login = query("SELECT * FROM members WHERE email='$email' AND password=SHA('$password')");
+  if ($row = mysqli_fetch_assoc($verify_login)) {
+    $_SESSION['user'] = $row['firstName'] . ' ' . $row['lastName'];
+    $_SESSION['id'] = $row['memberId'];
+    $_SESSION['email'] = $row['email'];
+    header("Refresh:0");
+    exit;
+  }
+  else {
+    echo "<script type=\"text/javascript\">
+      alert(\"Invalid Login\");
+      </script>";
+    echo "<meta http-equiv=\"refresh\" content=\"0; login.php\">";
+  }
+}
+
 if (isset($_SESSION['user'])) { // if the user is logged in
   if (isset($_POST['submit_logout'])) { // if the user logs out, destroy the session and refresh the page
     session_destroy();
