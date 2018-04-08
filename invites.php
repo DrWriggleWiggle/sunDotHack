acadaa6da38f6e21a651610ee2d26275e7accdf7
bull313 <blladi313@gmail.com>
2018-04-08 08:00:44 -0700

    debugging

--- invites.php
+++ invites.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+  if (!isset($_SESSION['id'])) {
+    echo "<meta http-equiv=\"refresh\" content=\"0; index.php\">";
+  }
+ ?>
+
+<head>
+  <?php require_once("head.php"); ?>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>My Calendar</title>
+  <!-- Bootstrap core CSS-->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Page level plugin CSS-->
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+</head>
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
@@ -172,3 +197,5 @@
    ?>
  </ul>
 </div>
+</body>
+</html>
