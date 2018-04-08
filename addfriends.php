951109b1f8424bc1ee468bdfe164108a44234843
bull313 <blladi313@gmail.com>
2018-04-08 02:21:55 -0700

    its the final countdown

--- addfriends.php
+++ addfriends.php
@@ -1,245 +1,69 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <?php require_once("head.php"); ?>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>My Calendar</title>
-  <!-- Bootstrap core CSS-->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom fonts for this template-->
-  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-  <!-- Page level plugin CSS-->
-  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin.css" rel="stylesheet">
-</head>
-
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="dashboard.html">Wriggle Social Calendar</a>
+    <a class="navbar-brand" href="index.html">Wriggle Social Calendar</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-          <a class="nav-link" href="dashboard.php">
-            <i class="fa fa-fw fa-dashboard"></i>
-            <span class="dashboard.php">My Calendar</span>
+	  <a class="nav-link" href="index.php">
+	    <i class="fa fa-fw fa-dashboard"></i>
+	    <span class="index.php">My Calendar</span>
+	  </a>
+	</li>
+  <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+  <a class="nav-link" href="index.php">
+  <i class="fa fa-fw fa-dashboard"></i>
+  <span class="invites.php">Invites</span>
+  </a>
+  </li>
+	<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+	  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
+	    <i class="fa fa-fw fa-wrench"></i>
+	    <span class="nav-link-text">Friends</span>
+	  </a>
+	  <ul class="sidenav-second-level collapse" id="collapseComponents">
+	    <li>
+	      <a href="friends.php">My Friends</a>
+	    </li>
+	    <li>
+	      <a href="addfriends.php">Add Friend</a>
+	    </li>
+	  </ul>
+	</li>
+      </ul>
+      <ul class="navbar-nav sidenav-toggler">
+        <li class="nav-item">
+          <a class="nav-link text-center" id="sidenavToggler">
+            <i class="fa fa-fw fa-angle-left"></i>
           </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-wrench"></i>
-            <span class="nav-link-text">Friends</span>
-          </a>
-          <ul class="sidenav-second-level collapse" id="collapseComponents">
-            <li>
-              <a href="friends.php">My Friends</a>
-            </li>
-            <li>
-              <a href="addfriends.php">Add Friend</a>
-            </li>
-          </ul>
         </li>
       </ul>
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-fw fa-envelope"></i>
-            <span class="d-lg-none">Messages
-              <span class="badge badge-pill badge-primary">12 New</span>
-            </span>
-            <span class="indicator text-primary d-none d-lg-block">
-              <i class="fa fa-fw fa-circle"></i>
-            </span>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="messagesDropdown">
-            <h6 class="dropdown-header">New Messages:</h6>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <strong>David Miller</strong>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end of the box so they don't overflow over to the sides!</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <strong>Jane Smith</strong>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <strong>John Doe</strong>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can discuss distribution.</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item small" href="#">View all messages</a>
-          </div>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-fw fa-bell"></i>
-            <span class="d-lg-none">Alerts
-              <span class="badge badge-pill badge-warning">6 New</span>
-            </span>
-            <span class="indicator text-warning d-none d-lg-block">
-              <i class="fa fa-fw fa-circle"></i>
-            </span>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="alertsDropdown">
-            <h6 class="dropdown-header">New Alerts:</h6>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <span class="text-success">
-                <strong>
-                  <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
-              </span>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <span class="text-danger">
-                <strong>
-                  <i class="fa fa-long-arrow-down fa-fw"></i>Status Update</strong>
-              </span>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <span class="text-success">
-                <strong>
-                  <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
-              </span>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item small" href="#">View all alerts</a>
-          </div>
+        <li class="nav-item">
+          <?php
+          // reminds the user who is logged in
+          $name = $_SESSION['user'];
+          echo "<h2>Logged in as $name.</h2>";
+          ?>
         </li>
         <li class="nav-item">
-          <form class="form-inline my-2 my-lg-0 mr-lg-2">
-            <div class="input-group">
-              <input class="form-control" type="text" placeholder="Search for...">
-              <span class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                  <i class="fa fa-search"></i>
-                </button>
-              </span>
+          <!-- Logout form -->
+          <form action="index.php" method="post">
+            <div>
+              <input type="submit" name="submit_logout" value="Logout">
             </div>
           </form>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
-            <i class="fa fa-fw fa-sign-out"></i>Logout</a>
         </li>
       </ul>
     </div>
   </nav>
   <div class="content-wrapper">
     <div class="container-fluid">
-      <!-- Example DataTables Card-->
-      <div class="card mb-3">
-        <div class="card-header">
-          <i class="fa fa-table"></i> Pending Friend Requests</div>
-        <div class="card-body">
-          <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-              <thead>
-                <tr>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Accept</th>
-                  <th>Ignore</th>
-                </tr>
-              </thead>
-              <tfoot>
-                <tr>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Accept</th>
-                  <th>Ignore</th>
-                </tr>
-                <?php
-                require_once("sql.php");
-                $friendRequests = getTable("friends WHERE friend2='" . $_SESSION['id'] . "' AND accepted='0';");
-                foreach ($friendRequests as $fr) {
-                  $member = getMemberById($fr['friend1']);
-                  echo "<tr>";
-                  echo "<th>" . $member['firstName'] . "</th>";
-                  echo "<th>" . $member['lastName'] . "</th>";
-                  echo "<th><form action='addfriends.php' method='post'>";
-                  echo "<input type='hidden' value='" . $member['memberId'] . "' name='friend1'>";
-                  echo "<input type='submit' value='Accept' name='submit_friend_request_accept'></th>";
-                  echo "<th><input type='submit' value='Reject' name='submit_friend_request_reject'></th>";
-                  echo "</form>";
-                  echo "</tr>";
-
-                if (isset($_POST['submit_friend_request_accept'])) { // if a friend request is ACCEPTED, set accepted to 1
-                  query(
-                    "UPDATE friends SET accepted='1'
-                    WHERE friend1='" . $_POST['friend1'] . "' AND friend2='" . $_SESSION['id'] . "';
-                    "
-                  );
-                } else if (isset($_POST['submit_friend_request_reject'])) { // if a friend request is REJECTED, remove the row in the db
-                  query(
-                    "DELETE FROM friends
-                    WHERE friend1='" . $_POST['friend1'] . "' AND friend2='" . $_SESSION['id'] . "';
-                    "
-                  );
-                }
-                  }
-                  ?>
-              </tbody>
-            </table>
-          </div>
-          <div>
-            <h4>Send Friend Request</h4>
-            <form action="addfriends.php" method="post">
-              <label>E-mail</label> <input type="text" name="email"> <br>
-              <input type="submit" name="submit_friend_request" value="Request Friendship">
-            </form>
-            <?php
-            require_once("sql.php");
-            if (isset($_POST['submit_friend_request'])) { // if the user just submitted a friend request
-              $friend1 = $_SESSION['id'];
-              $friend2 = -1;
-              $result = getTable("members WHERE email='" . $_POST['email'] . "'"); // look for account with requested e-mail
-              if (count($result) == 0) {
-                echo "<h5><em>Cannot find user with email " . $_POST['email'] . "</em></h5>";
-              } else {
-                $member = $result[0];
-                $friend2 = $member['memberId'];
-                if ($member['memberId'] == $_SESSION['id']) { // if requested e-mail is from signed-in account, throw an error message
-                  echo "<h5><em>Cannot send a friend request to yourself</em></h5>";
-                } else {
-                  // if a friendship or request is already present in the database, throw an error message
-                  $test = getTable("friends WHERE (friend1='" . $_SESSION['id'] . "' AND friend2='$friend2') OR (friend2='" . $_SESSION['id'] . "' AND friend1='$friend2');");
-                  if (count($test) > 0) {
-                    echo "<h5><em>Friendship already going on.</em></h5>";
-                  } else {
-                    // adds request to database
-                    query(
-                      "INSERT INTO friends (friend1, friend2, accepted)
-                      VALUES ('$friend1', '$friend2', '0');"
-                    );
-                    echo "<h5><em>Friendship requested from " . $member['firstName'] . ' ' . $member['lastName'] . "</em></h5>";
-                  }
-                }
-              }
-            }
-            ?>
-        </div>
+      <div class="row">
+        
       </div>
     </div>
     <!-- /.container-fluid-->
@@ -287,5 +111,3 @@
     <script src="js/sb-admin-datatables.min.js"></script>
   </div>
 </body>
-
-</html>
---
+++ invites.php
@@ -0,0 +1,144 @@
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+  <!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <a class="navbar-brand" href="index.html">Wriggle Social Calendar</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+	  <a class="nav-link" href="index.php">
+	    <i class="fa fa-fw fa-dashboard"></i>
+	    <span class="index.php">My Calendar</span>
+	  </a>
+	</li>
+  <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+  <a class="nav-link" href="index.php">
+  <i class="fa fa-fw fa-dashboard"></i>
+  <span class="invites.php">Invites</span>
+  </a>
+  </li>
+	<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+	  <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
+	    <i class="fa fa-fw fa-wrench"></i>
+	    <span class="nav-link-text">Friends</span>
+	  </a>
+	  <ul class="sidenav-second-level collapse" id="collapseComponents">
+	    <li>
+	      <a href="friends.php">My Friends</a>
+	    </li>
+	    <li>
+	      <a href="addfriends.php">Add Friend</a>
+	    </li>
+	  </ul>
+	</li>
+      </ul>
+      <ul class="navbar-nav sidenav-toggler">
+        <li class="nav-item">
+          <a class="nav-link text-center" id="sidenavToggler">
+            <i class="fa fa-fw fa-angle-left"></i>
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <?php
+          // reminds the user who is logged in
+          $name = $_SESSION['user'];
+          echo "<h2>Logged in as $name.</h2>";
+          ?>
+        </li>
+        <li class="nav-item">
+          <!-- Logout form -->
+          <form action="index.php" method="post">
+            <div>
+              <input type="submit" name="submit_logout" value="Logout">
+            </div>
+          </form>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="content-wrapper">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div>
+            <h3>Invitations</h3>
+            <ul>
+            <?php
+
+            require_once("sql.php");
+            if (isset($_POST['submit_invite_accept'])){ //if invite is accepted, set accepted to 1
+              query("UPDATE actions SET accepted=1 WHERE event='" . $_POST['event'] . "' AND member='" . $_SESSION['id'] . "';");
+            }
+            else if(isset($_POST['submit_invite_decline'])){ //if invite is declined, set accepted to 2
+              query("UPDATE actions SET accepted=2 WHERE event='" . $_POST['event'] . "' AND member='" . $_SESSION['id'] . "';");
+            }
+
+            //$invitations is an array of pending events associated with the current user
+            $invitations = getInvites($_SESSION['id']);
+            foreach ($invitations as $i) {
+              echo "<li>";
+              echo "Invitation to " . $i['name'];
+              echo "<form action='index.php' method='post'>";
+              echo "<div>";
+              echo "<input type='hidden' value='" . $i['eventId'] . "' name='event'>";
+              echo "<input type='submit' value='Accept' name='submit_invite_accept'>";
+              echo "<input type='submit' value='Decline' name='submit_invite_decline'>";
+              echo "</div>";
+              echo "</form>";
+              echo "</li>";
+            }
+             ?>
+           </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.container-fluid-->
+    <!-- /.content-wrapper-->
+    <footer class="sticky-footer">
+      <div class="container">
+        <div class="text-center">
+          <small>Copyright © Wriggle Social Calendar Corp 2018</small>
+        </div>
+      </div>
+    </footer>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fa fa-angle-up"></i>
+    </a>
+    <!-- Logout Modal-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a class="btn btn-primary" href="login.html">Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin.min.js"></script>
+    <!-- Custom scripts for this page-->
+    <script src="js/sb-admin-datatables.min.js"></script>
+  </div>
+</body>
