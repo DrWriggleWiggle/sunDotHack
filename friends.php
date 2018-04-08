3ff945c99b660ec0d4fcba7f332c833efa4b225b
bull313 <blladi313@gmail.com>
2018-04-08 09:18:41 -0700

    debugging

--- friends.php
+++ friends.php
@@ -124,7 +124,7 @@
               $friendSubmissions = getTable("friends WHERE friend1='" . $_SESSION['id'] . "' AND accepted='0';");
               foreach ($friendSubmissions as $fs) {
                 $member = getMemberById($fs['friend2']);
-                echo "<tr><td>";
+                echo "<tr colspan='5'><td>";
                 echo "Waiting on friend request sent to " . $member['firstName'] . ' ' . $member['lastName'];
                 echo "<form action='index.php' method='post'>";
                 echo "<div>";
