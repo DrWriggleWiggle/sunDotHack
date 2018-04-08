367d5011b8f1e01ff79ec440ca788c55f543109b
TymanLS <sin.tyman@yahoo.com>
2018-04-07 20:52:41 -0700

    There were two of them?

--- sql.php
+++ sql.php
@@ -60,11 +60,4 @@
       return $row;
     }
   }
-
-  function getEventById($val) {
-    $request = query("SELECT * FROM events WHERE eventId='$val'");
-    if ($row = mysqli_fetch_assoc($request)) {
-      return $row;
-    }
-  }
 ?>
